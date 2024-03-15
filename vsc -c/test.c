ce7dce1835b8a1e1887bada249a4d5fc87da7d68
kanye <3241583594@qq.com>
2024-03-15 23:26:48 +0800

    try

--- vsc -c/test.c
+++ vsc -c/test.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int a = 10 ;
-    double b = 12 ;
-
-    printf("%lf\n", a + b);
+    double A,B,C,D;
+    float a,b;
+    scanf("%lf,%lf",&A,&B);
+    C=A+B;
+    printf("%-10.6lf+%10.6lf=%-10.2lf\n",A,B,C);
+    a=(A*100+0.5)/100.0;
+    b=(B*100+0.5)/100.0;
+    D=a+b;
+    printf("%-10.6lf+%10.6lf=%-10.2lf\n",a,b,D);
+    return 0;
 }
