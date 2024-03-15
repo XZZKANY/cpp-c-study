4235a38f2782158e34e83d0b0faa3bed53c07887
kanye <3241583594@qq.com>
2024-03-15 21:26:55 +0800

    '

--- acwing/acwing-761.cpp
+++ acwing/acwing-761.cpp
@@ -16,3 +16,21 @@ int main()
     }
     printf("%d", count);
 }
+// c 语言
+/*#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[110];
+    
+    fgets(a, sizeof a ,stdin);
+    int count = 0;
+    for (int i = 0; i < strlen(a); i ++)
+    {
+        char b = a[i];
+        if (b >= '0' && b <= '9') count += 1;
+    }
+    printf("%d", count);
+}
+*/
