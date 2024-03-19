cf8e06416d7e8051f53a03ea00fbffcaaa82e782
kanye <3241583594@qq.com>
2024-03-19 23:15:11 +0800

    try

---
+++ acwing/acwing.773.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string str, substr, newstr1 = "", newstr2 = "";
+    cin >> str >> substr ;
+    
+    int maxindex= 0, maxcount = 0;
+    
+    for (int i = 0; str[i]; i ++)
+        if ((int)str[i] > maxcount){ 
+            maxindex = i;
+            maxcount = (int)str[i];
+        }
+    
+    for (int i = 0; i < maxindex; i ++) newstr1 += str[i]; 
+    for (int j = maxindex + 1; j <= str.size(); j ++) newstr2 += str[j];
+
+    cout << newstr1 + substr + newstr2 << '\n' ;
+
+    return 0;
+        
+
+}
