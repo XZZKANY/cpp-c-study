7c93830c1d5312b2b2d0f3c79823a50ef44f0c89
kanye <3241583594@qq.com>
2024-03-20 17:39:14 +0800

    try

--- acwing/acwing.773.cpp
+++ acwing/acwing.773.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 int main()
 {
-    string str, substr, newstr1 = "", newstr2 = "";
+    string str, substr; ;
     
-    while (cin >> str >> substr, str){
+    cin >> str >> substr ;
+     while (cin >> str >> substr ){
+        string newstr1 , newstr2 ;
+        int maxindex= 0, maxcount = 0;
     
-    int maxindex= 0, maxcount = 0;
+        for (int i = 0; str[i]; i ++)
+            if ((int)str[i] > maxcount){ 
+                maxindex = i;
+                maxcount = (int)str[i];
+            }
     
-    for (int i = 0; str[i]; i ++)
-        if ((int)str[i] > maxcount){ 
-            maxindex = i;
-            maxcount = (int)str[i];
-        }
-    
-    for (int i = 0; i < maxindex; i ++) newstr1 += str[i]; 
-    for (int j = maxindex + 1; j <= str.size(); j ++) newstr2 += str[j];
+        for (int i = 0; i <= maxindex; i ++) newstr1 += str[i]; 
+        for (int j = maxindex + 1; j <= str.size(); j ++) newstr2 += str[j];
 
-    cout << newstr1 + substr + newstr2 << '\n' ;
-
-    return 0;
-    }
+        cout << newstr1 + substr + newstr2 << '\n' ;
         
+    
+     }
+    return 0;    
 
 }
